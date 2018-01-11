95b38d3a7e7f87e41df2e1081599c689c75c7182
Allando <alla2634@edu.easj.dk>
2018-01-11 19:20:29 +0100

    Added to echo

--- SelfTests/echo.asm
+++ SelfTests/echo.asm
@@ -1,21 +1,53 @@
-section .data:
-    msg db "Please write stuff",$0A,0
-    len equ $ - msg
+section .data
+    welcome_msg     db "Hello, there", 0xa
+    welcome_len     equ $ - welcome_msg
+
+    instru_msg      db "Write somthing", 0xa
+    instru_len      equ $ - instru_msg
+
+    buff equ        2048
+
+    SYS_WRITE       equ 1
+    STD_IN          equ 1
+    SYS_EXIT        equ 60
+    EXIT_CODE       equ 0
+
+    NEW_LINE        db 0xa
 
 section .text
     global _start
 
 _start:
-    ;; Print option
-    mov     edx, len
-    mov     ecx, msg
-    mov     ebx, 1
+    ;; Print welcome message
     mov     eax, 4
-    int     0x80
+    mov     ebx, 1
+    mov     ecx, welcome_msg
+    mov     edx, welcome_len
+    syscall
 
-    mov     eax, 1
-    int     0x80
-
+    ;; Print instruction to user
+    mov     eax, 4
+    mov     ebx, 1
+    mov     ecx, instru_msg
+    mov     edx, instru_len
+    syscall
+    
     ;; TODO: Get user input
+    
 
     ;; TODO: Print user input
+
+get_user_input:
+    ; https://stackoverflow.com/questions/7006926/learning-assembly-echo-program-name
+    xor eax, eax
+    xor edi, edi
+    mov rsi, buffer
+    mov edx, 2048
+
+exit:
+    ; syscall number
+    mov     rax, SYS_EXIT
+    ; exit code
+    mov     rdi, EXIT_CODE
+    ; call sys_exit
+    syscall
