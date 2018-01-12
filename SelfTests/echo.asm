973af4a6b4d612943c02cb58e9a2a8c34772924c
Allando <alla2634@edu.easj.dk>
2018-01-12 02:18:40 +0100

    Changed arch to x64

--- SelfTests/echo.asm
+++ SelfTests/echo.asm
@@ -1,53 +1,31 @@
 section .data
-    welcome_msg     db "Hello, there", 0xa
-    welcome_len     equ $ - welcome_msg
+    welcome_msg     db "Hello, there", 0xa, 0xa
+    welcome_len     equ 14
 
-    instru_msg      db "Write somthing", 0xa
-    instru_len      equ $ - instru_msg
-
-    buff equ        2048
-
-    SYS_WRITE       equ 1
-    STD_IN          equ 1
-    SYS_EXIT        equ 60
-    EXIT_CODE       equ 0
-
-    NEW_LINE        db 0xa
+    instru_msg      db "Write something", 0xa
+    instru_len      equ 16
 
 section .text
     global _start
 
 _start:
     ;; Print welcome message
-    mov     eax, 4
-    mov     ebx, 1
-    mov     ecx, welcome_msg
-    mov     edx, welcome_len
+    mov     rax, 1
+    mov     rdi, 1
+    mov     rsi, welcome_msg
+    mov     rdx, welcome_len
     syscall
 
     ;; Print instruction to user
-    mov     eax, 4
-    mov     ebx, 1
-    mov     ecx, instru_msg
-    mov     edx, instru_len
+    mov     rax, 1 
+    mov     rdi, 1
+    mov     rsi, instru_msg
+    mov     rdx, instru_len
     syscall
-    
-    ;; TODO: Get user input
-    
 
-    ;; TODO: Print user input
-
-get_user_input:
-    ; https://stackoverflow.com/questions/7006926/learning-assembly-echo-program-name
-    xor eax, eax
-    xor edi, edi
-    mov rsi, buffer
-    mov edx, 2048
+    call exit
 
 exit:
-    ; syscall number
-    mov     rax, SYS_EXIT
-    ; exit code
-    mov     rdi, EXIT_CODE
-    ; call sys_exit
+    mov     rax, 60
+    mov     rdi, 0
     syscall
