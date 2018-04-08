a9e5070e82ec31aef67675d14fdfc7cdf9d661d7
Allando <alla2634@edu.easj.dk>
2018-04-08 13:30:50 +0200

    lala

--- SelfTests/Echo/simpleEchox86.asm
+++ SelfTests/Echo/simpleEchox86.asm
@@ -1,6 +1,12 @@
 section .data
-    wlcm_msg        db "Enter ya name, I will tell you what you are called", 0xa
+    
+    
+    wlcm_msg        db "Hello, there!", 0xa
     wlcmMsg_len     equ $ - wlcm_msg
+
+    req_msg        db "Enter ya name, I will tell you what you are called", 0xa
+    reqMsg_len     equ $ - req_msg
+
 
     disp_msg        db "You are called: "
     dimpMsg_len     equ $ - disp_msg
@@ -12,29 +18,37 @@ section .text
     global _start
 
 _start:
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, wlcm_msg 
-    mov edx, wlcmMsg_len
-    int 0x80
+    mov     eax, 4
+    mov     ebx, 1
+    mov     ecx, wlcm_msg 
+    mov     edx, wlcmMsg_len
+    int     0x80
 
-    mov eax, 3
-    mov ebx, 2
-    mov ecx, name
-    mov edx, 25
-    int 0x80
+    mov     cl, 10
+l1:
+    mov     eax, 4
+    mov     ebx, 1
+    mov     ecx, req_msg
+    mov     edx, reqMsg_len
 
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, disp_msg
-    mov edx, dimpMsg_len
-    int 0x80
+    mov     eax, 3
+    mov     ebx, 2
+    mov     ecx, name
+    mov     edx, 25
+    int     0x80
+    mov     eax, 4
+    mov     ebx, 1
+    mov     ecx, disp_msg
+    mov     edx, dimpMsg_len
+    int     0x80
+    mov     eax, 4
+    mov     ebx, 1
+    mov     ecx, name
+    mov     edx, 25
+    int     0x80
+    dec     cl
+    jnz     l1
 
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, name
-    mov edx, 25
-    int 0x80
 
     call _exit
 
